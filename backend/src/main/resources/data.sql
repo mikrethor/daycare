87e52cdb1750df0e01108b93e7be2ef7382a7709
mikrethor <mikrethor@gmail.com>
2017-12-12 21:46:30 -0500

    Cleaning SQL script

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -15,18 +15,6 @@ INSERT INTO USER_ROLE("USER_ID", "ROLE_ID") VALUES(2,1);
 INSERT INTO USER_ROLE("USER_ID", "ROLE_ID") VALUES(2,2);
 INSERT INTO USER_ROLE("USER_ID", "ROLE_ID") VALUES(3,3);
 
---insert into ADMIN("ID") values (1);
---insert into PARENT("ID","DAYCARE","FIRSTNAME", "LASTNAME") values (1,1,'Xavier','B');
---insert into PARENT("ID","DAYCARE","FIRSTNAME", "LASTNAME") values (2,1,'Bérengère','B');
-
---insert into EDUCATOR("ID","DAYCARE","FIRSTNAME", "LASTNAME") values (1,1,'Marie-Josée', 'YMCA');
---insert into EDUCATOR("ID","DAYCARE","FIRSTNAME", "LASTNAME") values (2,1,'Bérengère', 'CB');
---insert into EDUCATOR("ID","DAYCARE","FIRSTNAME", "LASTNAME") values (3,1,'Joe', 'Tribiani');
-
---insert into USER("ID", "LOGIN", "PASSWORD","SALT", "DAYCARE_ID","ADMIN_ID","EDUCATOR_ID","PARENT_ID") values (3, 'test@admin','test','salt',1,1,NULL,NULL);
---insert into USER("ID", "LOGIN", "PASSWORD","SALT", "DAYCARE_ID","ADMIN_ID","EDUCATOR_ID","PARENT_ID") values (2, 'test@educator','test','salt',1,NULL,1,NULL);
---insert into USER("ID", "LOGIN", "PASSWORD","SALT", "DAYCARE_ID","ADMIN_ID","EDUCATOR_ID","PARENT_ID") values (1, 'test@parent','test','salt',1,NULL,NULL,1);
-
 insert into CHILD("ID", "FIRSTNAME", "LASTNAME", "DAYCARE") values (1, 'Arthur','B',1);
 insert into CHILD("ID", "FIRSTNAME", "LASTNAME", "DAYCARE") values (2, 'Louis','B',1);
 
@@ -55,14 +43,6 @@ insert into SUMUP("ID", "ID_CHILD", "COMMENT", "MOOD", "APPETITE", "SLEEP", "DAY
 
 
 insert into NEED("ID","CODE") values (1,'TEST');
-
---ALTER SEQUENCE SEQ_ID_USER RESTART WITH 4;
---ALTER SEQUENCE SEQ_ID_DAYCARE RESTART WITH 2;
---ALTER SEQUENCE SEQ_ID_ADMIN RESTART WITH 2;
---ALTER SEQUENCE SEQ_ID_PARENT RESTART WITH 3;
---ALTER SEQUENCE SEQ_ID_EDUCATOR RESTART WITH 4;
---ALTER SEQUENCE SEQ_ID_CHILD RESTART WITH 3;
---ALTER SEQUENCE SEQ_ID_SUMUP RESTART WITH 21;
 
 
 
