23b7961eede76645096d1ef40d1b8a6bcdaf9850
mikrethor <mikrethor@gmail.com>
2017-12-17 20:49:15 -0500

    Adding findAll Child by Daycare

--- backend/src/main/kotlin/com/ablx/daycare/backend/repository/ChildRepository.kt
+++ backend/src/main/kotlin/com/ablx/daycare/backend/repository/ChildRepository.kt
@@ -2,5 +2,12 @@ package com.ablx.daycare.backend.repository
 
 import com.ablx.daycare.backend.entity.Child
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 
-internal interface ChildRepository : JpaRepository<Child, Long>
+internal interface ChildRepository : JpaRepository<Child, Long>{
+
+
+    @Query("select c from Child c where c.daycare.id=:idDaycare")
+    fun findAllByDaycare(@Param("idDaycare") id: Long): List<Child>
+}
--- backend/src/test/kotlin/com/ablx/daycare/backend/repository/ChildRepositoryTest.kt
+++ backend/src/test/kotlin/com/ablx/daycare/backend/repository/ChildRepositoryTest.kt
@@ -1,6 +1,7 @@
 package com.ablx.daycare.backend.repository
 
 import org.assertj.core.api.Assertions
+import org.junit.Assert
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
@@ -17,11 +18,26 @@ class ChildRepositoryTest {
     @Test
     fun findOne() {
         val child= repository.findOne(1L)
-        Assertions.assertThat("Arthur")
-                .isEqualTo(child.firstname)
-        Assertions.assertThat("B")
-                .isEqualTo(child.lastname)
-        Assertions.assertThat(1L)
-                .isEqualTo(child.daycare.Id)
+        Assertions.assertThat(child.firstname)
+                .isEqualTo("Arthur")
+        Assertions.assertThat(child.lastname)
+                .isEqualTo("B")
+        Assertions.assertThat(child.daycare.Id)
+                .isEqualTo(1L)
+    }
+
+    @Test
+    fun findAllByDaycare() {
+        val childs= repository.findAllByDaycare(1L)
+        Assertions.assertThat(childs.size)
+                .isEqualTo(2)
+
+        val child1=childs.toTypedArray()[0]
+        Assert.assertEquals("Arthur",child1.firstname)
+        Assert.assertEquals("B",child1.lastname)
+
+        val child2=childs.toTypedArray()[1]
+        Assert.assertEquals("Louis",child2.firstname)
+        Assert.assertEquals("B",child2.lastname)
     }
 }
