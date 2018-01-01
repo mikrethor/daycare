b19a3cb3581d04bc68fc3d62c3cf0f6c80c9813b
mikrethor <mikrethor@gmail.com>
2018-01-01 17:37:59 -0500

    Adding annotation LocalStorage

---
+++ frontend/src/app/annotations/local-storage.ts
@@ -0,0 +1,18 @@
+/**
+ * LocalStorage for storing string values
+ */
+export function LocalStorage(
+    target: Object, // The prototype of the class
+    decoratedPropertyName: string // The name of the property
+) {
+
+    // get and set methods
+    Object.defineProperty(target, decoratedPropertyName, {
+        get: function () {
+            return localStorage.getItem(decoratedPropertyName) || '';
+        },
+        set: function (newValue) {
+            localStorage.setItem(decoratedPropertyName, newValue);
+        }
+    });
+}
