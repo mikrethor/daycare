1b51d2ffee8d9e538016453f4f0ba8d8a28b043e
mikrethor <mikrethor@gmail.com>
2018-01-08 23:14:19 -0500

    When not found create a new sumup for the current day

--- backend/src/main/kotlin/com/ablx/daycare/backend/controller/SumupController.kt
+++ backend/src/main/kotlin/com/ablx/daycare/backend/controller/SumupController.kt
@@ -1,11 +1,13 @@
 package com.ablx.daycare.backend.controller
 
 import com.ablx.daycare.backend.entity.Sumup
+import com.ablx.daycare.backend.objects.Level
 import com.ablx.daycare.backend.repository.ChildRepository
 import com.ablx.daycare.backend.repository.SumupRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.format.annotation.DateTimeFormat
 import org.springframework.web.bind.annotation.*
+import java.text.SimpleDateFormat
 import java.util.*
 
 
@@ -22,13 +24,35 @@ internal class SumupController {
     fun findOne(@PathVariable(value="idDaycare")idDaycare: Long,
                 @PathVariable(value="idChild")idChild: Long,
                 @DateTimeFormat(pattern = "yyyy-MM-dd")
-                @PathVariable(value="day")day: Calendar) =
-            try{
-                sumupRepository.findOneByChildAndDay(idChild,day)}
-            catch (e:Exception){
-                System.err.println(e)
-            Sumup()
+                @PathVariable(value="day")day: Calendar):Sumup {
+
+        //TODO refactoring
+        try{
+            var sumup=sumupRepository.findOneByChildAndDay(idChild,day)
+            if(sumup == null){
+                sumup=Sumup()
+                sumup.child=childRepository.findOne(idChild)
+                sumup.day=GregorianCalendar()
+                val sdf = SimpleDateFormat("yyyy-MM-dd'Z'")
+                sumup.day.time = sdf.parse(sdf.format( sumup.day.time))
+
+
+                sumup.mood=Level.BAD
+                sumup.appetite=Level.BAD
+                sumup.sleep=Level.BAD
             }
+            return  sumup
+        }
+        catch (e:Exception){
+            System.err.println(e)
+            var sumup=Sumup()
+            sumup.child=childRepository.findOne(idChild)
+            sumup.day=GregorianCalendar()
+            sumup.mood=Level.BAD
+            sumup.appetite=Level.BAD
+            sumup.sleep=Level.BAD
+            return sumup
+        }}
 
     @GetMapping("/daycares/{idDaycare}/childs/{idChild}/sumups")
     fun findAll(@PathVariable(value="idDaycare")idDaycare: Long,
