e8465dbbe910dae83fa0759c9e95a966e9b3d0ce
mikrethor <mikrethor@gmail.com>
2018-03-08 20:49:57 -0500

    Using Jasmine and Karma to test

--- frontend/src/app/components/application/app.component.spec.ts
+++ frontend/src/app/components/application/app.component.spec.ts
@@ -1,34 +1,115 @@
-/* tslint:disable:no-unused-variable */
+import {Router, Routes} from "@angular/router";
+import {fakeAsync, TestBed, tick} from "@angular/core/testing";
+import {RouterTestingModule} from "@angular/router/testing";
+import {AppComponent} from "./app.component";
+import {EditSumupsComponent} from "../sumups/edit-sumups.component";
+import {UserComponent} from "../user/user.component";
+import {EducatorComponent} from "../educator/educator.component";
+import {AdminEditChildComponent} from "../daycareadmin/edit-child.component";
+import {DaycareAdminComponent} from "../daycareadmin/daycareadmin.component";
+import {RegisterComponent} from "../register/register.component";
+import {AdminEditUserComponent} from "../daycareadmin/edit-user.components";
+import {UnderNavbarComponent} from "../undernavbar/undernavbar.component";
+import {LogoutComponent} from "../logout/logout.component";
+import {LoginComponent} from "../login/login.component";
+import {AdminUserComponent} from "../daycareadmin/user.component";
+import {AdminChildComponent} from "../daycareadmin/child.component";
+import {ParentComponent} from "../parent/parent.component";
+import {NavbarComponent} from "../navbar/navbar.component";
+import {MaterialModule} from "../../material.module";
+import {BrowserModule} from "@angular/platform-browser";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+import {APP_BASE_HREF, Location} from '@angular/common';
+import {DateService} from "../../services/date-service";
+import {SumupService, SumupServiceImpl} from "../../services/sumup-service";
+import {ChildService, ChildServiceImpl} from "../../services/child-service";
+import {ParentService, ParentServiceImpl} from "../../services/parent-service";
+import {LOCALE_ID} from "@angular/core";
+import {EducatorService, EducatorServiceImpl} from "../../services/educator-service";
+import {LoginService} from "../../services/login-service";
+import {UserService} from "../../services/user-service";
+import {RoleService} from "../../services/role-service";
+import {ConstantsService} from "../../services/constants-service";
+import {DaycareService, DaycareServiceImpl} from "../../services/daycare-service";
+import {LoggerModule, NgxLoggerLevel} from "ngx-logger";
 
-import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+const routes: Routes = [
+    {path: '', redirectTo: 'login', pathMatch: 'full'},
+    {path: 'login', component: LoginComponent},
+    {path: 'logout', component: LogoutComponent, pathMatch: 'full'},
+    {path: 'register', component: RegisterComponent, pathMatch: 'full'},
+    {
+        path: 'daycare/:idDaycare', component: UnderNavbarComponent, children: [
+            {path: 'user/:username', component: UserComponent, outlet: 'undernavbar',},
+            {path: 'parent/:idParent', component: ParentComponent, outlet: 'undernavbar',},
+            {path: 'educator/:idEducator', component: EducatorComponent, outlet: 'undernavbar',},
+            {path: 'children/:idChild/sumups', component: EditSumupsComponent, outlet: 'undernavbar',},
 
-describe('AppComponent', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+            {path: 'admin/:idAdmin', component: DaycareAdminComponent, outlet: 'undernavbar',},
+            {path: 'admin/:idAdmin/children', component: AdminChildComponent, outlet: 'undernavbar',},
+            {path: 'admin/:idAdmin/children/create', component: AdminEditChildComponent, outlet: 'undernavbar',},
+            {path: 'admin/:idAdmin/children/:idChild/edit', component: AdminEditChildComponent, outlet: 'undernavbar',},
+            {path: 'admin/:idAdmin/users', component: AdminUserComponent, outlet: 'undernavbar'},
+            {path: 'admin/:idAdmin/users/create', component: AdminEditUserComponent, outlet: 'undernavbar',},
+            {path: 'admin/:idAdmin/users/:idUser/edit', component: AdminEditUserComponent, outlet: 'undernavbar',},
+        ]
+    },
+];
+
+describe('Router: App', () => {
+
+    let location: Location;
+    let router: Router;
+    let fixture;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [RouterTestingModule.withRoutes(routes), MaterialModule,
+                BrowserModule,
+                FormsModule,
+                HttpClientModule,
+                MaterialModule,
+                BrowserAnimationsModule,
+
+                LoggerModule.forRoot({
+                    serverLoggingUrl: 'http://localhost:9200/daycare/logs',
+                    level: NgxLoggerLevel.DEBUG,
+                    serverLogLevel: NgxLoggerLevel.OFF
+                }),
+            ],
+            declarations: [
+
+                AppComponent, LoginComponent, LogoutComponent, UserComponent, EducatorComponent, ParentComponent,
+                DaycareAdminComponent, NavbarComponent, UnderNavbarComponent, EditSumupsComponent, AdminChildComponent,
+                AdminEditChildComponent, RegisterComponent,
+                AdminEditUserComponent, AdminUserComponent
+            ],
+            providers: [LoginService, ConstantsService, UserService, DateService, RoleService,
+                //   { provide: LocationStrategy, useClass: HashLocationStrategy, },
+                {provide: LOCALE_ID, useValue: 'fr'},
+                {provide: APP_BASE_HREF, useValue: '/',},
+                {provide: ChildService, useClass: ChildServiceImpl},
+                {provide: DaycareService, useClass: DaycareServiceImpl},
+                {provide: EducatorService, useClass: EducatorServiceImpl},
+                {provide: ParentService, useClass: ParentServiceImpl},
+                {provide: SumupService, useClass: SumupServiceImpl},
+
+            ],
+
+        });
+
+        router = TestBed.get(Router);
+        location = TestBed.get(Location);
+
+        fixture = TestBed.createComponent(AppComponent);
+        router.initialNavigation();
     });
-    TestBed.compileComponents();
-  });
 
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
-
-  it(`should have as title 'app works!'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
-  }));
-
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
-  }));
+    it('navigate to "" redirects you to /login', fakeAsync(() => {
+        router.navigate(['']);
+        tick();
+        expect(location.path()).toBe('');
+    }));
 });
